83774ffab764454b9992d8dd0c2d6fe79bdba432
Mateusz Pietrzyk <G00383118@atu.ie>
2022-10-07 12:56:23 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+//import react
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Content } from './components/content';
+import { Header } from './components/header';
+import { Footer} from './components/footer';
+import Navbar from 'react-bootstrap/Navbar';
+import  Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 
-function App() {
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+
+//new class created 
+class App extends React.Component {
+  render(){
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/read">Read</Nav.Link>
+            <Nav.Link href="/create">Create</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path='/' element={<Content></Content>}></Route>
+        <Route path='/read' element={<Header></Header>}></Route>
+      </Routes>
+      {/* <Header></Header>
+     <Content></Content>
+  <Footer></Footer> */}
     </div>
+   </Router>
   );
+  }
 }
 
 export default App;
